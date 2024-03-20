b973f51d81b68995688eb7d317177937d7527929
Вадим Кутьин <vadim.kutyn@gmail.com>
2024-03-20 19:34:19 +0100

    Add Shop.Entities project, add Product Entity

---
+++ Shop.Entities/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Shop.Entities
+{
+	public class Product(int id, int categoryId, string title)
+	{
+		public int Id => id;
+		public int CategoryId => categoryId;
+		public string Title => title;
+		public string Description { get; set; } = "";
+	}
+}
