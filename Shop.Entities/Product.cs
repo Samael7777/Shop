faa6af587d2311f757b538c0a2df9a2c891b875e
Вадим Кутьин <vadim.kutyn@gmail.com>
2024-03-23 22:48:51 +0100

    Add "price" property to Product

--- Shop.Entities/Product.cs
+++ Shop.Entities/Product.cs
@@ -1,9 +1,10 @@
 ﻿namespace Shop.Entities;
 
-public class Product(int id, int categoryId, string title)
+public class Product(int id, int categoryId, string title, decimal price)
 {
 	public int Id => id;
 	public int CategoryId => categoryId;
 	public string Title => title;
 	public string Description { get; set; } = "";
+	public decimal Price => price;
 }
